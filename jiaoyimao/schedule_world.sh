f73acae38107ad1b38e73dbb042efde09be9f026
attitude <937316808@qq.com>
2017-09-24 00:00:26 +0800

    world

--- jiaoyimao/schedule_world.sh
+++ jiaoyimao/schedule_world.sh
@@ -1,2 +1,2 @@
-mysqldump --defaults-extra-file=/etc/mysql/mysql.conf.d/mysqld.cnf world info > info.sql
+mysqldump --defaults-extra-file=/etc/mysql/mysql.conf.d/mysqld.cnf world > /home/project/toolkits/jiaoyimao/world.sql
 
